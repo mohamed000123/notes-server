98529a7ad7f00cd985e26c07563d2800242e3eb0
Fady Attia <fady.attia@callvita-eg.com>
2023-11-26 20:53:21 +0200

    project initialization

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { sequelize } from "./models/dbConnection.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+sequelize
+  .authenticate()
+  .then(() => {
+    app.listen("8000", () => {
+      console.log("server is running on port 8000");
+    });
+  })
+  .catch((e) => {
+    console.log("error connecting to db", e);
+  });
+// middelwares
+app.use(cookieParser());
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
