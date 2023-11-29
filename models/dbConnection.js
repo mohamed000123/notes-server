64b9561b36aaa027bf75ad703f97ae9ad3ab534d
mohamed amin <fady.attia@callvita-eg.com>
2023-11-29 18:33:23 +0200

    uploading .env file

--- models/dbConnection.js
+++ models/dbConnection.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 export const sequelize = new Sequelize(
-  "guest_note",
+  process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
